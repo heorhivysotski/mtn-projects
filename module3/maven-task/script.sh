810d7bf58d43243475a0c95eb644d32fa7d56d0f
heorhivysotski <heorhi_vysotski@epam.com>
2017-02-23 15:18:08 +0300

    Update script.sh

--- module3/maven-task/script.sh
+++ module3/maven-task/script.sh
@@ -1,2 +1,2 @@
 cd /target
-zip -r heorhivysotski-1.0.tar.gz /home/heorhi_vysotski/mtn-projects/module3/maven-task/target/heorhivysotski-1.0*.jar
+tar -zcvf heorhivysotski-1.0.jar.tar.gz heorhivysotski-1.0*.jar

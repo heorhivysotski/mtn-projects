d7b5cf7d09b5f3913edebf2486e7fa6d239b51aa
Heorhi Vysotski <heorhi_vysotski@epam.com>
2017-02-27 15:57:11 +0300

    Update UnitTest.java

--- module3/maven-task/src/test/java/com/test/UnitTest.java
+++ module3/maven-task/src/test/java/com/test/UnitTest.java
@@ -8,17 +8,4 @@ public class UnitTest {
    public void test(){
      Project.test();
    }
-   
-   public void test1(){
-     Project.test1();
-   }
-   public void test2(){
-     Project.test2();
-   }
-   public void test3(){
-     Project.test3();
-   }
-   public void test4(){
-     Project.test4();
-   }
 }

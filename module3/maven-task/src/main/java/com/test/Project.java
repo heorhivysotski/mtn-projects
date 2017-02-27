13db2baaea7637972c61a4f489204bb65241d06c
Heorhi Vysotski <heorhi_vysotski@epam.com>
2017-02-27 13:46:23 +0300

    Update Project.java

--- module3/maven-task/src/main/java/com/test/Project.java
+++ module3/maven-task/src/main/java/com/test/Project.java
@@ -8,9 +8,29 @@ class Project {
   public static void test() {
     System.out.println("test");
   }
+  //test1
+   public static void test() {
+    System.out.println("test1");
+  }
+  //test2
+   public static void test() {
+    System.out.println("test2");
+  }
+  //test3
+   public static void test() {
+    System.out.println("test3");
+  }
+  //test4
+   public static void test() {
+    System.out.println("test4");
+  }  
   
   public static void main(String[] args) {
     System.out.println("Hellow MTN");
+    System.out.println("Some");
+    System.out.println("Custom");
+    System.out.println("Strings");
+    System.out.println("Output");
   }
 
 }
